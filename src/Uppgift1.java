1214ce471b5433bb34c78a4644a659b1c447fe1d
ludde016 <ludde.swala@gmail.com>
2018-09-08 16:46:20 +0200

    Commit #5
    Klar med uppgiften. Klurade lite på det bästa sättet att fixa med formatteringen, tittade lite online men gjorde ändå på det sättet som jag visste att det skulle funka på i slutet.
    
    Är inte heller 100% säker om jag skicka in alla commits i exakt ordning men förhoppningsvis så funkar det!
    
    Mvh,
    Ludvig Swala

--- src/Uppgift1.java
+++ src/Uppgift1.java
@@ -14,7 +14,31 @@ public class Uppgift1 {
 		
 		String Alder = input.next();
 		
+		System.out.println("Vad är din adress?");
+		
+		String Adress = input.next();
+		
+		System.out.println("Vad är ditt postnummer?");
+		
+		String Postnummer = input.next();
+		
+		System.out.println("I vilken stad bor du?");
+		
+		String Stad = input.next();
+		
+		System.out.println("Vad är ditt telefonnummer");
+		
+		String Nummer = input.next();
+		
 		input.close();
+		
+		System.out.println("Namn:     " + Namn);
+		
+		System.out.println("Ålder:    " + Alder);
+		
+		System.out.println("Adress:   " + Adress +"\r"+ "          " + Postnummer + " " + Stad);
+		
+		System.out.println("Nummer:   " + Nummer );
 		
 	}
 
