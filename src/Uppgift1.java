b5a6c81a73f6dcb058f76b771df449cb6b7d3104
ludde016 <ludde.swala@gmail.com>
2018-09-11 12:44:48 +0200

    Commit #8
    Jag gjorde det enklare genom att använda input.nextLine() istället för endast input.next(). På detta sätt blir koden kortare och enklare.

--- src/Uppgift1.java
+++ src/Uppgift1.java
@@ -8,39 +8,35 @@ public class Uppgift1 {
 		
 		System.out.println("Vad heter du?");
 		
-		String Namn = input.next();
-		
-		String Efternamn = input.next();
+		String Namn = input.nextLine();
 		
 		System.out.println("Hur gammal är du?");
 		
-		String Alder = input.next();
+		String Alder = input.nextLine();
 		
 		System.out.println("Vad är din adress?");
 		
-		String Adress = input.next();
-		
-		String Adresstal = input.next();
+		String Adress = input.nextLine();
 		
 		System.out.println("Vad är ditt postnummer?");
 		
-		String Postnummer = input.next();
+		String Postnummer = input.nextLine();
 		
 		System.out.println("I vilken stad bor du?");
 		
-		String Stad = input.next();
+		String Stad = input.nextLine();
 		
 		System.out.println("Vad är ditt telefonnummer");
 		
-		String Nummer = input.next();
+		String Nummer = input.nextLine();
 		
 		input.close();
 		
-		System.out.println("Namn:     " + Namn + " " + Efternamn);
+		System.out.println("Namn:     " + Namn);
 		
 		System.out.println("Ålder:    " + Alder);
 		
-		System.out.println("Adress:   " + Adress + " " + Adresstal +"\r"+ "          " + Postnummer + " " + Stad);
+		System.out.println("Adress:   " + Adress +"\r"+ "          " + Postnummer + " " + Stad);
 		
 		System.out.println("Nummer:   " + Nummer );
 		
