d5d17d10509b277ff2bd9b149b56c609ffa22577
ludde016 <ludde.swala@gmail.com>
2018-09-14 15:57:51 +0200

    Commit #2
    Det här kan verka som mycket men det jag har gjort är att sätta in 11 variabler varav 10 är de hetal som man skriver in och den 11:e är det största talet som man skriver in. Nu ska jag göra en 12:e variabel som är det minsta talet som man skriver in. Det finns säkert bättre sätt att göra detta på (som med en array) men i med att jag missade dagens lektion (fredag 14:e september) så kan jag endast använda det jag redan vet för att klara av detta. Ska kanske efter se om det finns ett bättre sät men funkar det så funkar det

--- src/Uppgift2.java
+++ src/Uppgift2.java
@@ -7,6 +7,38 @@ public class Uppgift2 {
 		Scanner input = new Scanner (System.in);
 		
 		System.out.println("Skriv in 10 heltal: ");
+		
+		double largest = 0; 
+		
+		double tal1 = input.nextDouble(); 
+		if (tal1 > 0) {largest = tal1;}
+		
+		double tal2 = input.nextDouble(); 
+		if (tal2 > tal1) {largest = tal1;}
+		
+		double tal3 = input.nextDouble(); 
+		if (tal3 > tal2) {largest = tal1;}
+		
+		double tal4 = input.nextDouble(); 
+		if (tal4 > tal3) {largest = tal1;}
+		
+		double tal5 = input.nextDouble(); 
+		if (tal5 > tal4) {largest = tal1;}
+		
+		double tal6 = input.nextDouble(); 
+		if (tal6 > tal5) {largest = tal1;}
+		
+		double tal7 = input.nextDouble(); 
+		if (tal7 > tal6) {largest = tal1;}
+		
+		double tal8 = input.nextDouble();
+		if (tal8 > tal7) {largest = tal1;}
+		
+		double tal9 = input.nextDouble(); 
+		if (tal9 > tal8) {largest = tal1;}
+		
+		double tal0 = input.nextDouble(); 
+		if (tal0 > tal9) {largest = tal1;}
 	
 	}
 
