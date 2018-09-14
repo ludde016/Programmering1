b494dc9b9c84a3e9c392b424a88c5bc5bf1ea66b
ludde016 <ludde.swala@gmail.com>
2018-09-14 15:45:02 +0200

    Commit #1
    Då har vi en grund för det vi vill göra

---
+++ src/Uppgift2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class Uppgift2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Skriv in 10 heltal: ");
+	
+	}
+
+}
