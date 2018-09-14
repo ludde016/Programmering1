854c4be340f1255fd19f6da88b8a04aca2439d9d
ludde016 <ludde.swala@gmail.com>
2018-09-14 16:05:22 +0200

    Commit #3
    Nu har jag lagt till ett vilkor som säger om det nya talet är mindre än det minsta talet så blir det talet det nya minsta talet (and so on). Jag fixade också så att samma vilkor fast med det största talet faktiskt fungerar ny då jag, utan att tänka mig för, bara kopierade samma rad av kod och ändra på 3 bokstaver varje gång.
    
    Det du ser i slutet är både en del av koden men också ett test för att se så att det fungerar (och det gör det också! Än så länge....).

--- src/Uppgift2.java
+++ src/Uppgift2.java
@@ -12,33 +12,46 @@ public class Uppgift2 {
 		
 		double tal1 = input.nextDouble(); 
 		if (tal1 > 0) {largest = tal1;}
+		double smallest = tal1;
 		
 		double tal2 = input.nextDouble(); 
-		if (tal2 > tal1) {largest = tal1;}
+		if (tal2 > largest) {largest = tal1;}
+		if (tal2 < smallest) {smallest = tal2;}
 		
 		double tal3 = input.nextDouble(); 
-		if (tal3 > tal2) {largest = tal1;}
+		if (tal3 > largest) {largest = tal3;}
+		if (tal3 < smallest) {smallest = tal3;}
 		
 		double tal4 = input.nextDouble(); 
-		if (tal4 > tal3) {largest = tal1;}
+		if (tal4 > largest) {largest = tal4;}
+		if (tal4 < smallest) {smallest = tal4;}
 		
 		double tal5 = input.nextDouble(); 
-		if (tal5 > tal4) {largest = tal1;}
+		if (tal5 > largest) {largest = tal5;}
+		if (tal5 < smallest) {smallest = tal5;}
 		
 		double tal6 = input.nextDouble(); 
-		if (tal6 > tal5) {largest = tal1;}
+		if (tal6 > largest) {largest = tal6;}
+		if (tal6 < smallest) {smallest = tal6;}
 		
 		double tal7 = input.nextDouble(); 
-		if (tal7 > tal6) {largest = tal1;}
+		if (tal7 > largest) {largest = tal7;}
+		if (tal7 < smallest) {smallest = tal7;}
 		
 		double tal8 = input.nextDouble();
-		if (tal8 > tal7) {largest = tal1;}
+		if (tal8 > largest) {largest = tal8;}
+		if (tal8 < smallest) {smallest = tal8;}
 		
 		double tal9 = input.nextDouble(); 
-		if (tal9 > tal8) {largest = tal1;}
+		if (tal9 > largest) {largest = tal9;}
+		if (tal9 < smallest) {smallest = tal9;}
 		
 		double tal0 = input.nextDouble(); 
-		if (tal0 > tal9) {largest = tal1;}
+		if (tal0 > largest) {largest = tal0;}
+		if (tal0 < smallest) {smallest = tal0;}
+		
+		System.out.println(largest);
+		System.out.println(smallest);
 	
 	}
 
