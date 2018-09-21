04408ea55b4c9e4e6e62a0661d492a5b91d612b0
ludde016 <ludde.swala@gmail.com>
2018-09-21 21:24:31 +0200

    Commit #1
    Hittils har jag bara skapat en ny fil med main method, en scanner och två variablet vilket är de 2 heltal man skriver in

---
+++ src/R2Uppgift.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class R2Uppgift {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		int tal1 = input.nextInt();
+		int tal2 = input.nextInt();
+		
+		
+	}
+}
