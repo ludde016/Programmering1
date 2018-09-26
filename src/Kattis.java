12c4abf4997cc7539902e5862645861cda28b00a
ludde016 <ludde.swala@gmail.com>
2018-09-26 16:08:17 +0200

    Ingen betydelse för uppgifter, en egen fil

---
+++ src/Kattis.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Kattis {
+
+		public static void main(String[] args) {
+			
+			Scanner input = new Scanner (System.in);
+			
+			String ord = input.nextLine();
+			
+			System.out.println(ord);
+			
+			input.close();
+		}
+}
