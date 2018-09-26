4daa337751581da5ece71cf8fb9cf1e6d6f37246
ludde016 <ludde.swala@gmail.com>
2018-09-26 22:15:24 +0200

    Commit #1
    Nu commitade jag inte under processen men det jag har gjort äratt skapa 2 variabler som är H (första input) och M (andra input) varav dessa står för Hours and Minutes.
    
    Det jag gjorde därefter åt var att lägga till en variabeln som var M minus 45, vilket fick få namnet NyM.
    
    Därefterk om ett villkor som sa att om NyM är under noll (negativ) så minskas H med 1 och vi skapar en ny variabel som blir NyM + 60 (så om M - 45 skulle bara 30 - 45 = -15 så skulle NyNyM bli -15 + 60).
    
    Annars om M - 45 går utan problem så skriver vi bara ut svaret... punkt

--- src/Spavanc.java
+++ src/Spavanc.java
@@ -7,7 +7,20 @@ public class Spavanc {
 		Scanner input = new Scanner (System.in); 
 		
 		int H = input.nextInt(); 
-		int M = input.nextInt(); 
+		int M = input.nextInt();
+		
+		int NyM = (M - 45); 
+		
+		if (NyM < 0  ) { 
+			int NyH = (H - 1); 
+			int NyNyM = (NyM + 60); 
+			if (NyH < 0) {NyH = (23);}
+			
+			System.out.println(NyH + " " + NyNyM );
+			}
+		else System.out.println(H + " " + NyM);
+		
+	
 		
 		
 		
