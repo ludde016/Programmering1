ebbeaa1b66032dff2d8a91eea2ff65a5076121f6
ludde016 <ludde.swala@gmail.com>
2018-12-12 16:16:59 +0100

    Commit #6
    Hoppade över reverse rot för en stund för att göra klart Östgötska för den var gör enkel

--- src/Arrayer_Uppgifter5_ReverseRot.java
+++ src/Arrayer_Uppgifter5_ReverseRot.java
@@ -4,6 +4,7 @@ public class Arrayer_Uppgifter5_ReverseRot {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner input = new Scanner(System.in); 
+		String intag = input.nextLine();
 	}
 }
---
+++ src/Arrayer_uppgift6Ostgotska.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Arrayer_uppgift6Ostgotska {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String intag = input.nextLine();
+		String[] ord = intag.split(" ");
+		double fall = 0;
+		
+		for (int i = 0; i < ord.length; i++) {
+			if (ord[i].contains("ae") == true) {
+				fall++;
+			}
+			
+		}
+		double procent = fall / ord.length;
+		if (procent == 0.4 || procent > 0.4) {
+			System.out.println("dae ae ju traeligt va");
+		}
+		else {
+			System.out.println("haer talar vi rikssvenska");
+		}
+	}
+
+}
