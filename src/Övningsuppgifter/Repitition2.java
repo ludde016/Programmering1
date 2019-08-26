7fbab630f4a00f17f0b9477fd34021e9bd573bd5
ludde016 <ludde.swala@gmail.com>
2019-08-26 10:20:39 +0200

    Commit #4
    Började på uppgift 2 och har skapat scanner och en sträng som tar emot ord från input.

---
+++ src/Övningsuppgifter/Repitition2.java
@@ -0,0 +1,12 @@
+package Övningsuppgifter;
+
+import java.util.Scanner;
+
+public class Repitition2 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String ord = input.nextLine();   
+	}
+
+}
