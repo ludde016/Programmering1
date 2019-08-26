6730ae25bf524a18778297cedfd8a359cdcf9ca4
ludde016 <ludde.swala@gmail.com>
2019-08-26 10:04:48 +0200

    Commit #1,5
    Glömde spara

--- src/Övningsuppgifter/Repition1.java
+++ src/Övningsuppgifter/Repition1.java
@@ -4,7 +4,11 @@ public class Repition1 {
 	
 	public static void main(String[] args) {
 		
-		int[] array = {1,2,3,4,5,6}; 
+		int[] array = {1,2,3,4,5,6};
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			
+		}
 	}
 
 }
