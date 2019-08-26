5c90df96456b77d2cd3cc24afd0ee793869e3859
ludde016 <ludde.swala@gmail.com>
2019-08-26 10:08:59 +0200

    Commit #2
    Skapade en for-loop som adderar alla värden i arrayen till sum och skriver sedan ut det efter for-loopen

--- src/Övningsuppgifter/Repition1.java
+++ src/Övningsuppgifter/Repition1.java
@@ -7,8 +7,9 @@ public class Repition1 {
 		int[] array = {1,2,3,4,5,6};
 		int sum = 0;
 		for (int i = 0; i < array.length; i++) {
-			
+			sum += sum + array[i]; 
 		}
+		System.out.println(sum);
 	}
 
 }
